917c7603626d9c4c8009e47badc732e2ec5adedf
michael-cowled <michael.cowled@gmail.com>
2025-08-07 16:19:56 +1000

    Updated messaging

--- R/get_cid_with_fallbacks.R
+++ R/get_cid_with_fallbacks.R
@@ -18,7 +18,7 @@ get_cid_only_with_fallbacks <- function(name, smiles = NA, cid_cache_df) {
 
   # --- 1. If a cached CID is found, return it immediately ---
   if (nrow(cached_entry) > 0 && !is.na(cached_entry$CID[1])) {
-    message(paste("  [CACHE HIT] CID found for:", name, " (CID:", cached_entry$CID[1], ")"))
+    message(paste("  [CACHE HIT] CID found for '", name, "' (CID:", cached_entry$CID[1], ")"))
     return(list(CID = cached_entry$CID[1], cache = cid_cache_df))
   }
 
@@ -30,22 +30,22 @@ get_cid_only_with_fallbacks <- function(name, smiles = NA, cid_cache_df) {
 
   # Try SMILES lookup
   if (is.na(resolved_cid) && !is.na(smiles) && smiles != "") {
-    message(paste("  Name lookup failed. Trying SMILES:", smiles))
+    message(paste("  Name lookup failed for '", name, "'. Trying SMILES:", smiles))
     resolved_cid <- get_pubchem_lite(smiles, "smiles", "cids")
   }
 
   # Try synonym lookup
   if (is.na(resolved_cid)) {
-    message(paste("  Name and SMILES failed. Trying synonym search for:", name))
+    message(paste("  Name and SMILES failed for '", name, "'. Trying synonym search..."))
     resolved_cid <- get_pubchem_lite(name, "synonym", "cids")
   }
 
   # --- 3. Update Cache based on lookup result ---
   if (!is.na(resolved_cid)) {
-    message(paste("  [PUBCHEM] Found CID: ", resolved_cid))
+    message(paste("  [PUBCHEM] Found CID for '", name, "': ", resolved_cid))
     new_entry <- data.frame(LookupName = name, CID = resolved_cid, stringsAsFactors = FALSE)
   } else {
-    message(paste("  [PUBCHEM] No CID found for:", name))
+    message(paste("  [PUBCHEM] No CID found for '", name, "'."))
     # Add a failed entry to cache
     new_entry <- data.frame(LookupName = name, CID = -1, stringsAsFactors = FALSE)
   }
