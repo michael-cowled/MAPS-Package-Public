58ad4bffc47d4e52102bacd88174b89825ba7ef3
michael-cowled <michael.cowled@gmail.com>
2025-10-03 13:18:19 +1000

    checking if pass3 activated

--- R/standardise_annotation.R
+++ R/standardise_annotation.R
@@ -159,7 +159,7 @@ standardise_annotation <- function(data,
 
   # Check if the PubChem lookup feature is enabled
   if (pubchem_lookup_enabled) {
-
+    message("Starting Pass 3: Pubchem Lookup for Standardisation:")
     # Check if the required 'jsonlite' package is installed and load it
     if (!requireNamespace("jsonlite", quietly = TRUE)) {
       warning("PubChem lookup is enabled, but the 'jsonlite' package is not installed. Skipping lookup.")
