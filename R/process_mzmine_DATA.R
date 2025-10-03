eff3166cae75559019cb34f35cf481678a602c83
michael-cowled <michael.cowled@gmail.com>
2025-10-03 10:14:38 +1000

    a bug

--- R/process_mzmine_DATA.R
+++ R/process_mzmine_DATA.R
@@ -136,7 +136,7 @@ process_mzmine_data <- function(mzmine.annotations, mzmine.data, gnps.prob,
   return(list(
     processed.data = mzmine.data,
     lipids.data = lipids,
-    annotations.data = mzmine.annotations,
+    annotations.data = mzmine.annotations.final,
     initial.mzmine.data = initial.mzmine.data,
     cid.cache = cid_cache_df,
     lipids.file = lipids.file
